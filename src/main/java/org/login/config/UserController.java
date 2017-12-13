248341f3c1119982049c0c3b501f984922b3672a
jorge12387 <jorge.aniez@gmail.com>
2017-12-13 14:11:57 -0400

    revoke token

--- src/main/java/org/login/config/UserController.java
+++ src/main/java/org/login/config/UserController.java
@@ -2,15 +2,40 @@ package org.login.config;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class UserController {
+    @Autowired
+    private TokenStore tokenStore;
 
     @RequestMapping("/user/me")
     public Principal user(Principal principal) {
         System.out.println(principal);
         return principal;
+    }
+    @RequestMapping("/oauth/revoke-token")
+    @ResponseStatus(HttpStatus.OK)
+    public void revokeToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null) {
+            String tokenValue = authHeader.replace("Bearer", "").trim();
+            OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+            if (accessToken != null) {
+                if (accessToken.getRefreshToken() != null) {
+                    tokenStore.removeRefreshToken(accessToken.getRefreshToken());
+                }
+                this.tokenStore.removeAccessToken(accessToken);
+            }
+        }
     }
 }
--- src/main/java/org/login/persistence/UserDetailsServiceImpl.java
+++ src/main/java/org/login/persistence/UserDetailsServiceImpl.java
@@ -38,7 +38,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
 
         return new org.springframework.security.core.userdetails.User(
-                user.getEmail(), user.getPassword(), user.isEnabled(), true, true,
+                user.getUsername(), user.getPassword(), user.isEnabled(), true, true,
                 true, getAuthorities(roleDAO.findRoleForUser(username)));
     }
 
